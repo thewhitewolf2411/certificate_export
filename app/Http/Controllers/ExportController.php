75d3550bfa3df79dc79ed6b71700b18f11d8f686
Haris Muslić <39027423+thewhitewolf2411@users.noreply.github.com>
2020-10-08 17:31:18 +0200

    first commit

---
+++ app/Http/Controllers/ExportController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExportController extends Controller
+{
+    public function showIndex(){
+        return view('welcome');
+    }
+
+    public function exportPdf(){
+
+    }
+}
